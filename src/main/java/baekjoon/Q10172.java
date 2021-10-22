6b4c5ee7206c3fd9c6f79885afc16d26fafdcc0b
yoongeonung <yoongeonung@gmail.com>
2021-10-22 17:44:25 +0900

    feat : Baekjoon Q10172

---
+++ src/main/java/baekjoon/Q10172.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+public class Q10172 {
+    public static void main(String[] args) {
+        System.out.println("""
+                |\\_/|
+                |q p|   /}
+                ( 0 )""\"\\
+                |"^"`    |
+                ||_/=\\\\__|
+                """);
+    }
+}
