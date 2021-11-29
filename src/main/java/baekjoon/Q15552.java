a80d7290e2f4a901fbaa9fac227e0aaa7c2642f0
yoongeonung <yoongeonung@gmail.com>
2021-11-29 15:40:11 +0900

    feat : Baekjoon Q15552

---
+++ src/main/java/baekjoon/Q15552.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Q15552 {
+    /*
+     *   빠른 A+B
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int count = Integer.parseInt(br.readLine());
+        for (int i = 0; i < count; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            bw.write(String.valueOf(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) + "\n");
+        }
+        bw.flush();
+    }
+}
