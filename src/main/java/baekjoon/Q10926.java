4b1a77aa6595229936d51dd8716f070936da6c51
yoongeonung <yoongeonung@gmail.com>
2022-04-01 07:05:59 +0900

    feat : Q10926

---
+++ src/main/java/baekjoon/Q10926.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q10926 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        System.out.println(line + "??!");
+    }
+}
