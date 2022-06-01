89d9509509812c87685ac4ec09a22c437914d02f
yoongeonung <yoongeonung@gmail.com>
2022-06-01 12:47:09 +0900

    INF : 01-04 Another Solution

--- src/main/java/inflearn/chap1/ReverseString.java
+++ src/main/java/inflearn/chap1/ReverseString.java
@@ -3,12 +3,42 @@ package inflearn.chap1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReverseString {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    solutionOne(br);
+//    solutionOne(br);
+    solutionTwo(br);
+
+
+  }
+
+  private static void solutionTwo(BufferedReader br) throws IOException {
+    List<String> reversedString = new ArrayList<>();
+    int cnt = Integer.parseInt(br.readLine());
+    String[] strings = new String[cnt];
+    for (int i = 0; i < cnt; i++) {
+      strings[i] = br.readLine();
+    }
+    for (String string : strings) {
+      char[] chars = string.toCharArray();
+      int lt = 0;
+      int rt = chars.length - 1;
+      while (lt < rt) {
+        char tmp = chars[lt];
+        chars[lt] = chars[rt];
+        chars[rt] = tmp;
+        lt++;
+        rt--;
+      }
+      reversedString.add(String.valueOf(chars));
+    }
+    for (String s : reversedString) {
+      System.out.println(s);
+    }
   }
 
   private static void solutionOne(BufferedReader br) throws IOException {
