037b609f998ade1641d028120c41f5eccd14fa9b
yoongeonung <yoongeonung@gmail.com>
2022-08-30 15:10:51 +0900

    etc

---
+++ inflearn/chap02/ch0203/rps.go
@@ -0,0 +1,18 @@
+package ch0203
+
+import (
+	"fmt"
+)
+
+/*
+1. 가위
+2. 바위
+3. 보
+비길경우 : D를 출력
+*/
+
+func RockPaperScssiors() {
+
+	var test = [][]int{{1, 2}, {3, 4, 5}}
+	fmt.Println(test[1][2]) //5
+}
--- inflearn/chap02/main.go
+++ inflearn/chap02/main.go
@@ -1,9 +1,7 @@
 package main
 
-import (
-	"chap02/ch0202"
-)
+import "chap02/ch0203"
 
 func main() {
-	ch0202.SeeableStudent()
+	ch0203.RockPaperScssiors()
 }
